4e2f8672c7e070dfeb27a6b1ad7b961a402b6a8f
tOnzm <tonselaman@gmail.com>
2024-10-17 22:46:41 +0700

    ds

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   nitro: {
     preset: "node-server",
   },
+  // ตั้งค่าเบื้องต้น
+  ssr: true, // ใช้ server-side rendering
+
+  // ตั้งค่า baseURL ถ้าต้องการ
+  router: {},
+
+  // ตั้งค่าคุณสมบัติอื่นๆ ที่จำเป็น
 });
